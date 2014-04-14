4ffa6f6f933a599727e0d629d71c1965199c91ce
Lindsay Neubauer <neubauer@cs.columbia.edu>
2014-04-14 11:00:25 -0400

    merging

--- Kamino/src/edu/columbia/cs/psl/kamino/runtime/ControlLogger.java
+++ Kamino/src/edu/columbia/cs/psl/kamino/runtime/ControlLogger.java
@@ -5,25 +5,25 @@ public class ControlLogger {
 		System.out.println("You called doTest: " + whoDidtest);
 	}
 
-	public static void logEdgeControl(String className, String methodName, String methodDescriptor, int bbFrom, int bbTo) {
+	public static void logEdgeControl(String className, String methodName, String methodDescriptor, String frame, int bbFrom, int bbTo) {
 		// FIXME: LAN - switch to logging instead of printing
-		System.out.println("LogEdgeControl: " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " To:" + bbTo);
+		System.out.println("1  LogEdgeControl: " + frame + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " To:" + bbTo);
 	}
 
 	public static void logEdgeControl(boolean branchTaken, String className, String methodName, String methodDescriptor, int bbFrom, int bbTaken,
 	        int bbNotTaken) {
 		// FIXME: LAN - switch to logging instead of printing
-		System.out.println("LogEdgeControl: " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " bbTaken:" + bbTaken
+		System.out.println("2  LogEdgeControl: JUMP " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " bbTaken:" + bbTaken
 		        + " bbNotTaken:" + bbNotTaken + " Taken:" + branchTaken);
 	}
 
 	public static void logEdgeReadData(String className, String methodName, String methodDescriptor, int bbFrom, int bbTo) {
 		// FIXME: LAN - switch to logging instead of printing
-		System.out.println("LogEdgeReadData: " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " To:" + bbTo);
+		System.out.println("3  LogEdgeReadData: " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " To:" + bbTo);
 	}
 
 	public static void logEdgeWriteData(String className, String methodName, String methodDescriptor, int bbFrom, int bbTo) {
 		// FIXME: LAN - switch to logging instead of printing
-		System.out.println("LogEdgeWriteData: " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " To:" + bbTo);
-	}  
+		System.out.println("4  LogEdgeWriteData: " + className + "." + methodName + methodDescriptor + "  From:" + bbFrom + " To:" + bbTo);
+	}
 }
