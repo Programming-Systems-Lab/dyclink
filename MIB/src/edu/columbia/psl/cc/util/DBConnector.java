8828c630533e7708241ca2057149daaf5fb1202a
Fang-hsiang Su <fs2455@columbia.edu>
2015-02-18 03:19:43 -0500

    Line number mapping
    Former-commit-id: 28520bbe1288ba90849e3e0917fcaac3f86bc9aa

--- MIB/src/edu/columbia/psl/cc/util/DBConnector.java
+++ MIB/src/edu/columbia/psl/cc/util/DBConnector.java
@@ -1,23 +1,57 @@
 package edu.columbia.psl.cc.util;
 
 import java.io.Console;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 
+import com.google.gson.reflect.TypeToken;
+
 import edu.columbia.psl.cc.config.MIBConfiguration;
 import edu.columbia.psl.cc.pojo.HotZone;
+import edu.columbia.psl.cc.pojo.GraphTemplate;
+import edu.columbia.psl.cc.pojo.InstNode;
 
 public class DBConnector {
 	
-	private Logger logger = Logger.getLogger(DBConnector.class);
+	private static Logger logger = Logger.getLogger(DBConnector.class);
+	
+	private static String graphRepo = "/Users/mikefhsu/Mike/Research/ec2/mib_sandbox/";
+	
+	private static TypeToken<GraphTemplate> graphToken = new TypeToken<GraphTemplate>(){};
+	
+	private static File searchFile(List<String> possibleDirs, final String fileEnd) {
+		FilenameFilter ff = new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				// TODO Auto-generated method stub
+				return name.endsWith(fileEnd);
+			}
+		};
+		
+		for (String dir: possibleDirs) {
+			File f = new File(dir);
+			
+			File[] files = f.listFiles(ff);
+			if (files.length > 0) {
+				return files[0];
+			}
+		}
+		return null;
+	}
 	
 	public boolean probeDB(String baseurl, String username, String password) {
 		if (baseurl == null || username == null || password == null)
@@ -159,21 +193,135 @@ public class DBConnector {
 		}
 	}
 	
+	public static void analyzeResult(String url, String username, String password, int compId) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connect = DriverManager.getConnection(url, username, password);
+			
+			String compQuery = "SELECT * FROM comp_table " + " WHERE comp_id = " + compId;
+			PreparedStatement compStmt = connect.prepareStatement(compQuery);
+			ResultSet compResult = compStmt.executeQuery();
+			
+			String lib1 = null, lib2 = null;
+			if (compResult.first()) {
+				lib1 = compResult.getString("lib1");
+				lib2 = compResult.getString("lib2");
+			} else {
+				System.err.println("Empty comparison query result");
+				System.exit(-1);
+			}
+			List<String> possibleDirs = new ArrayList<String>();
+			possibleDirs.add(graphRepo + lib1);
+			possibleDirs.add(graphRepo + lib2);
+			System.out.println("Possible graph locations: " + possibleDirs);
+			
+			/*String query = "SELECT rt.* FROM result_table rt " +
+					"INNER JOIN (SELECT sub, sid, target, MAX(similarity) as sim " +
+					"FROM result_table " +
+					"WHERE comp_id=? and seg_size >=20 and target NOT LIKE '%ejml%' and sub NOT LIKE '%ejml%'" +
+					"GROUP BY sub, sid, target) max_rec " +
+					"ON rt.sub = max_rec.sub and rt.sid = max_rec.sid and rt.target = max_rec.target and rt.similarity = max_rec.sim " +
+					"ORDER BY rt.sub, rt.sid, rt.target, rt.tid, rt.similarity;";*/
+			
+			String query = "SELECT rt.* FROM result_table rt " +
+					"INNER JOIN (SELECT sub, sid, target, t_start_caller, t_centroid_caller, t_end_caller, MAX(similarity) as sim " +
+					"FROM result_table " +
+					"WHERE comp_id=? and seg_size >=20 and target NOT LIKE '%ejml%' and sub NOT LIKE '%ejml%' and similarity >= 0.8 " +
+					"GROUP BY sub, sid, target, t_start_caller, t_centroid_caller, t_end_caller) max_rec " +
+					"ON rt.sub = max_rec.sub and rt.sid = max_rec.sid and rt.target = max_rec.target and rt.t_start_caller = max_rec.t_start_caller and rt.t_centroid_caller = max_rec.t_centroid_caller and rt.t_end_caller = max_rec.t_end_caller and rt.similarity = max_rec.sim " +
+					"ORDER BY rt.sub, rt.sid, rt.target, rt.tid, rt.t_start_caller, rt.t_centroid_caller, rt.t_end_caller, rt.similarity;";
+			
+			PreparedStatement pStmt = connect.prepareStatement(query);
+			pStmt.setInt(1, compId);
+			ResultSet result = pStmt.executeQuery();
+			
+			HashMap<String, TreeSet<Integer>> graphCache = new HashMap<String, TreeSet<Integer>>();
+			HashMap<String, GraphTemplate> targetCache = new HashMap<String, GraphTemplate>();
+			while (result.next()) {
+				//Get graph object
+				String subId = result.getString("sid").replace("-", ":") + ".json";
+				String subName = result.getString("sub");
+				String targetId = result.getString("tid").replace("-", ":") + ".json";
+				String targetName = result.getString("target");
+				String tStartInst = result.getString("t_start");
+				int segSize = result.getInt("seg_size");
+				
+				TreeSet<Integer> subTrace = null;
+				if (!graphCache.containsKey(subId)) {
+					File subFile = searchFile(possibleDirs, subId);
+					//System.out.println("Test id: " + subId);
+					GraphTemplate subGraph = GsonManager.readJsonGeneric(subFile, graphToken);
+					//System.out.println("Test graph name: " + subGraph.getMethodName());
+					
+					subTrace = new TreeSet<Integer>();
+					for (InstNode inst: subGraph.getInstPool()) {
+						subTrace.add(inst.getLinenumber());
+					}
+					graphCache.put(subId, subTrace);
+				} else {
+					subTrace = graphCache.get(subId);
+				}
+				
+				GraphTemplate targetGraph = null;
+				if (!targetCache.containsKey(targetId)) {
+					//System.out.println("Target id: " + targetId);
+					File targetFile = searchFile(possibleDirs, targetId);
+					targetGraph = GsonManager.readJsonGeneric(targetFile, graphToken);
+					GraphConstructor reconstructor = new GraphConstructor();
+					reconstructor.reconstructGraph(targetGraph, true);
+					//System.out.println("Target graph name: " + targetGraph.getMethodName());
+					targetCache.put(targetId, targetGraph);
+				} else {
+					targetGraph = targetCache.get(targetId);
+				}
+				
+				List<InstNode> segments = GraphUtil.sortInstPool(targetGraph.getInstPool(), true);
+				int counter = 0;
+				boolean startRecord = false;
+				TreeSet<Integer> targetTrace = new TreeSet<Integer>();
+				for (InstNode s: segments) {
+					if (s.toString().equals(tStartInst)) {
+						startRecord = true;
+					}
+					
+					if (startRecord) {
+						targetTrace.add(s.callerLine);
+						counter++;
+						
+						if (counter == segSize) {
+							break ;
+						}
+					}
+				}
+				
+				System.out.println("Test graph: " + subName + " " + subId);
+				System.out.println("Test line trace: " + subTrace);
+				
+				System.out.println("Target graph: " + targetName + " " + targetId);
+				System.out.println("Target line trace: " + targetTrace);
+				System.out.println();
+				
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
 	public static void main(String args[]) {
 		Console console = System.console();
 		if (console == null) {
-			System.err.println("Null consol");
+			System.err.println("Null console");
 			System.exit(1);
 		}
 		
-		//String url = console.readLine("DB url: ");
-		//String username = console.readLine("DB username: ");
 		String url = MIBConfiguration.getInstance().getDburl();
 		String username = MIBConfiguration.getInstance().getDbusername();
+		System.out.println("DB URL: " + url);
+		
 		char[] passArray = console.readPassword("DB password: ");
 		String password = new String(passArray);
-		DBConnector connector = new DBConnector();
-		System.out.println("Prob db: " + connector.probeDB(url, username, password));
+		
+		DBConnector.analyzeResult(url, username, password, 25);
 	}
 	
 	public static class Comparison {
