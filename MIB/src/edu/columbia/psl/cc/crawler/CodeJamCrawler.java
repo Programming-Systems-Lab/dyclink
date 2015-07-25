37a4a62ff6c01e7e2e717186f965c7048b31a6ba
Fang-hsiang Su <fs2455@columbia.edu>
2015-07-25 16:26:33 -0400

    Unzip in the fly
    Former-commit-id: 133993d286c5b1113a8fabcb710d5ffdc6a37f1d

--- MIB/src/edu/columbia/psl/cc/crawler/CodeJamCrawler.java
+++ MIB/src/edu/columbia/psl/cc/crawler/CodeJamCrawler.java
@@ -1,13 +1,21 @@
 package edu.columbia.psl.cc.crawler;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,6 +26,52 @@ public class CodeJamCrawler {
 	public static final String baseUrl = "http://www.go-hero.net/jam/";
 	
 	public static final String matchString = "http://code.google.com/codejam/contest/scoreboard/do?cmd=GetSourceCode";
+	
+	public static int writeFileToDest(ZipInputStream zipStream, String filePath) {
+		try {
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+			byte[] bytesIn = new byte[1024];
+			int read = 0;
+			while ((read = zipStream.read(bytesIn)) != - 1) {
+				bos.write(bytesIn, 0, read);
+			}
+			bos.close();
+			System.out.println("Save file to " + filePath);
+			return 1;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public static void writeFile(InputStream inputStream, File destFile) {
+		try {
+			ReadableByteChannel rbc = Channels.newChannel(inputStream);
+			FileOutputStream fos = new FileOutputStream(destFile);
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			fos.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static int unpackArchive(File tmpFile, File javaFile) {
+		try {
+			if (javaFile.exists()) {
+				javaFile.delete();
+			}
+			
+			ZipFile zipFile = new ZipFile(tmpFile);
+			for (Enumeration entries = zipFile.entries(); entries.hasMoreElements();) {
+				ZipEntry entry = (ZipEntry)entries.nextElement();
+				writeFile(zipFile.getInputStream(entry), javaFile);
+			}
+			return 1;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return 0;
+	}
 	
 	public static void main(String[] args) {
 		try {
@@ -66,10 +120,6 @@ public class CodeJamCrawler {
 			}
 			
 			Document codeJamHome = Jsoup.connect(finalUrl).get();
-			//System.out.println("Show elements");
-			/*for (Element e: codeJamHome.getAllElements()) {
-				System.out.println(e.toString());
-			}*/
 			
 			int userCounter = 0;
 			for (Element e: codeJamHome.select("a[href]")) {
@@ -84,18 +134,18 @@ public class CodeJamCrawler {
 					System.out.println(userName);
 					
 					URL codeUrl = new URL(link);
-					ReadableByteChannel rbc = Channels.newChannel(codeUrl.openStream());
-					File codeZipFile = new File(problemDir + "/" + userName + ".zip");
-					System.out.println("Check roundDir: " + roundDir);
-					if (!codeZipFile.exists()) {
-						codeZipFile.createNewFile();
-					}
-					FileOutputStream fos = new FileOutputStream(codeZipFile);
-					fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
 					
-					System.out.println("Save file to: " + codeZipFile.getAbsolutePath());
+					//ReadableByteChannel rbc = Channels.newChannel(codeUrl.openStream());
+					File tmpFile = File.createTempFile("arc", ".zip", problemDir);
+					tmpFile.deleteOnExit();
+					//FileOutputStream fos = new FileOutputStream(tmpFile);
+					//fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+					writeFile(codeUrl.openStream(), tmpFile);
 					
-					userCounter++;
+					File javaFile = new File(problemDir + "/" + userName + ".java");
+					userCounter += unpackArchive(tmpFile, javaFile);
+					
+					System.out.println("Save file to: " + javaFile.getAbsolutePath());
 				}
 			}
 			System.out.println("Total code files: " + userCounter);
